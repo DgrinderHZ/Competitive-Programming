de3d7c0e3bbfc132d9c0dd5516f6e1d6a7476a98
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-01 18:45:12 +0100

    Create A.java

---
+++ Codeforces/Newcomers Training/Sheet #1 (Data type - Conditions)/A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Main{
+    public static void main(String[] args){
+        String s;
+        Scanner in = new Scanner(System.in);
+        
+        s = in.nextLine();
+        
+        System.out.println("Hello, " + s);
+    }
+}
