ce094e78b5e4ffc9931b8a8ff92f1ed5f6e6c1b9
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-02 16:26:59 +0100

    Create C.cpp

---
+++ Codeforces/Newcomers Training/Sheet #1 (Data type - Conditions)/C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+ 
+#include <iostream>
+ using namespace std;
+int main ()
+{
+  //freopen ("input.txt","r",stdin);
+  //freopen ("output.txt","w",stdin);
+  int a,b;
+ 
+  cin>> a>> b;
+  cout<<"sum = "<<a+b<<endl;
+ 
+  //fclose (stdin);
+  return 0;
+}
