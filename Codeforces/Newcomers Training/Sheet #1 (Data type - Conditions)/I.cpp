455da0d66f0afd335f1c455bcf25af7116972584
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-02 16:34:27 +0100

    Create I.cpp

---
+++ Codeforces/Newcomers Training/Sheet #1 (Data type - Conditions)/I.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+//Macros
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < b; i++)
+ 
+int main ()
+{
+  //freopen ("input.txt","r",stdin);
+  //freopen ("output.txt","w",stdout);
+ 
+  /* Code here */
+  
+  double a, p = 3.14159;
+  cin>>a;
+  printf("Area = %.9f",a*p*a); 
+  //fclose (stdin);
+  //fclose (stdout);
+  return 0;
+}
+ 
