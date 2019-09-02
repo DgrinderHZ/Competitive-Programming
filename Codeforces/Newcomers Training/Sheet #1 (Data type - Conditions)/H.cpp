e06ff8218fad2d9ba43237a482686e6b2cfdf233
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-02 16:31:45 +0100

    Create H.cpp

---
+++ Codeforces/Newcomers Training/Sheet #1 (Data type - Conditions)/H.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+//Macros
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < b; i++)
+ 
+int main ()
+{
+  //freopen ("input.txt","r",stdin);
+  //freopen ("output.txt","w",stdout);
+ 
+  /* Code here */
+  int a;
+  cin>>a;
+  cout<< a/365 <<" years\n"
+      << (a % 365)/30 <<" months\n"
+      << (a % 365)%30 <<" days"; 
+  //fclose (stdin);
+  //fclose (stdout);
+  return 0;
+}
