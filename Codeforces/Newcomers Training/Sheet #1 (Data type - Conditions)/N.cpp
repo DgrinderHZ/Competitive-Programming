4a83e4d79176db2c243640d0521c9fdd4a5dc9f7
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-02 16:38:52 +0100

    Create N.cpp

---
+++ Codeforces/Newcomers Training/Sheet #1 (Data type - Conditions)/N.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+//Macros
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < b; i++)
+ 
+int main ()
+{
+  //freopen ("input.txt","r",stdin);
+  //freopen ("output.txt","w",stdout);
+ 
+  /* Code here */
+  
+  char a; cin>>a;
+  
+  if(a < 97 ){
+  	cout<<"IS CAPITAL";
+  }else{
+  	cout<<"IS SMALL";
+  }
+  
+  //fclose (stdin);
+  //fclose (stdout);
+  return 0;
+}
+ 
