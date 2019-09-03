f0d199ff935d231573a22d6ea64cb942c529ca2d
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-03 13:03:37 +0100

    Create C.java

---
+++ Codeforces/Newcomers Training/Sheet #2 (Loops)/C.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        
+        Scanner in = new Scanner(System.in);
+        
+        
+        long b = in.nextLong();
+        
+    
+        // To Do: Try it using a loop!
+        System.out.print(b*(b+1)/2);
+    }
+}
