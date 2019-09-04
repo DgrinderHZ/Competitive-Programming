6ac4e76f3aa96c56e9b3ef0310c7cd8dc1c08a91
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-04 20:22:35 +0100

    Create A - Create A New String.cpp

---
+++ Codeforces/Newcomers Training/Sheet #4 (Strings)/A - Create A New String.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+ 
+//Macros
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < b; i++)
+ 
+int main ()
+{
+  //freopen ("input.txt","r",stdin);
+  //freopen ("output.txt","w",stdout);
+ 
+  /* Code here */
+  string a,b;
+  getline(cin, a);
+  getline(cin, b);
+  cout<< a +" "+b;
+ 
+  //fclose (stdin);
+  //fclose (stdout);
+  return 0;
+}
+ 
+ 
