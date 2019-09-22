7bc76ba8927a44d58f1a3eb56ff46d8fb5d7d5f1
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-22 17:12:31 +0100

    Create A - Winner.cpp

---
+++ Codeforces/A - Winner.cpp
@@ -0,0 +1,73 @@
+// https://codeforces.com/contest/2/problem/A
+// WA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Macros
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < (b); i++)
+#define prec setprecision(n)
+#define endl '\n'
+
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef struct{
+	string nm;
+	int scr;
+}Round;
+
+int main() {
+ #ifndef ONLINE_JUDGE
+  	freopen ("input.txt","r",stdin);
+  	//freopen ("output.txt","w",stdout);
+  #endif
+  
+  int n;
+  int m = -1002;
+  vector<Round> record;
+  map<string, int> roundw, final;
+  
+  cin>> n; 
+  while(n--){
+  	Round t;
+  	cin>> t.nm >> t.scr; 
+  	
+  	roundw[t.nm] += t.scr;
+  	record.pb(t);
+  }
+  // find the max
+  for(auto it = roundw.begin(); it != roundw.end(); it++){
+  	pair<string, int> temp = *it;
+  	if(roundw[temp.F] > m){
+  		m = roundw[temp.F];
+	  }
+  }
+  for(auto rnd: record){
+  	
+  	final[rnd.nm] += rnd.scr;
+  	if(final[rnd.nm] == m && roundw[rnd.nm] == m){
+  		cout<< rnd.nm;
+  		return 0;
+	  }
+  }
+  
+
+  return 0;
+}
