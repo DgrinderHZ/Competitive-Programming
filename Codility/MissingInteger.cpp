8df8a1c86842b500599ab9d8bcb16800bb965346
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-29 00:23:55 +0100

    Update MissingInteger.cpp

--- Codility/MissingInteger.cpp
+++ Codility/MissingInteger.cpp
@@ -6,11 +6,13 @@
 
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
-    vector<int> f(100009, 0);
+    vector<int> f(10000009, 0);
     unsigned int i;
-    for(i = 0; i < A.size(); i++) if(A[i] > 0) f[A[i]]++;
+    for(i = 0; i < A.size(); i++) 
+        if(A[i] > 0) f[A[i]]++;
+        else continue;
     
-    for(i=1; i <= A.size(); i++) if(f[i] == 0) return i;
+    for(i=1; i <= 10000002; i++) if(f[i] == 0) return i;
     
     return i;
 }
