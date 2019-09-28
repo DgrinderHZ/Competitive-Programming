3daa692156f09fa9b24b69dc22f3483b3ba2fdd9
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-28 19:29:21 +0100

    Create 677A - Vanya and Fence.cpp

---
+++ Codeforces/677A - Vanya and Fence.cpp
@@ -0,0 +1,63 @@
+#include <cmath>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//Macros
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < (b); i++)
+#define prec setprecision(n)
+#define endl '\n'
+
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef unsigned long long ull;
+typedef long double ld;
+
+
+int main ()
+{
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+  #endif // ONLINE_JUDGE
+
+  int n, h, mn = 0, a;
+  cin >> n >> h;
+  while(n--){
+    cin >> a;
+    if(a <= h) mn++;
+    else mn += 2;
+  }
+
+  cout << mn;
+
+  return 0;
+}
