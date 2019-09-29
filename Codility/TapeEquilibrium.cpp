7ec7438db34ef5c1d6221292f9137d0f1ead4b10
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-29 22:17:59 +0100

    Create TapeEquilibrium.cpp

---
+++ Codility/TapeEquilibrium.cpp
@@ -0,0 +1,25 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++14 (g++ 6.2.0)
+    int n = A.size();
+    vector<int> pre(n, 0);
+    pre[0] = A[0];
+    
+    for(int i=1; i < n; i++){
+        pre[i] = A[i] + pre[i-1];
+    }
+    
+    int mn = 1000000000;
+    for(int i=1; i < n-1; i++){
+        int cur = abs(pre.back() - pre[i] - pre[i]);
+        if(cur < mn) mn = cur;
+    }
+    
+    return mn;
+    
+}
