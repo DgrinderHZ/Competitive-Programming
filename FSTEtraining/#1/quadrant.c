391024e40d9074100851ce14c9adc5a6150705da
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-10-27 23:00:53 +0100

    Create quadrant.c

---
+++ FSTEtraining/#1/quadrant.c
@@ -0,0 +1,31 @@
+//https://open.kattis.com/contests/rnmdj2/problems/quadrant
+#include <stdio.h>
+
+
+int main ()
+{
+  //freopen ("input.txt","r",stdin);
+  //freopen ("output.txt","w",stdout);
+
+
+  int x, y;
+  scanf("%d", &x);
+  scanf("%d", &y);
+  if(x>0){
+    if(y>0){
+        printf("1");
+      }else{
+        printf("4");
+      }
+  }else{
+    if(y>0){
+        printf("2");
+      }else{
+        printf("2");
+      }
+  }
+   
+  //fclose (stdin);
+  //fclose (stdout);
+  return 0;
+}
